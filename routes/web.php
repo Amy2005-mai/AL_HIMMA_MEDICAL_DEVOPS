150b3636d5a6c2512cd437a8e6c4d60c5b214780
email@exemple.com <myname@.com>
2026-03-13 01:21:59 +0100

    commit fichier route web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/ajout', \App\Http\Controllers\ProduitController::class . '@index');
+Route::get('/produits/{id}/edit', [\App\Http\Controllers\ProduitController::class, 'edit'])
+    ->name('produits.edit');
+Route::post('/save', \App\Http\Controllers\ProduitController::class . '@store');
+Route::put('/produits/{id}', [\App\Http\Controllers\ProduitController::class , 'update'])->name('produits.update');
+Route::get('/produits', [\App\Http\Controllers\ProduitController::class, 'lecture'])->name('produits.lecture');
